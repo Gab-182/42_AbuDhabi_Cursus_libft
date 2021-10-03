47f186b77cba3f3a085fd4267b7481a506ebb3fb
Gabdoush <ghaith181994@hotmail.com>
2021-10-04 02:25:41 +0400

    correcting ft_memccpy after finding some errors while testing.

--- libft/string.h/ft_memccpy.c
+++ libft/string.h/ft_memccpy.c
@@ -1,14 +1,20 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memccpy.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gabdoush <gabdoush@student.42abudhabi      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/23 00:26:10 by gabdoush          #+#    #+#             */
-/*   Updated: 2021/09/23 08:09:57 by gabdoush         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/*
+ * #include <string.h>
+ *
+ * ft_memccpy(): copy string until character (c) found.
+ *
+ * DESCRIPTION:
+ * ============
+ * The memccpy() function copies the bytes from string (src) to string (dst).
+ * If the character (c)(converted to unsigned char) occurs in the string (src),
+ * the copy stops.
+ *
+ * Return:
+ *=======
+ * a pointer to the byte after the copy of the character(c) in the string (dst)
+ * is returned.
+ * Otherwise, (len) bytes are copied, and a NULL pointer is returned.
+ */
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -21,23 +27,27 @@ void 	*ft_memccpy(void *dst, const void *src, int c, size_t len)
 	// Check if dst and src are == to NULL.
 	if(!dst && !src)
 	{
-		return (NULL);
+		return (0);
 	}
 	i = 0;
 	while (i < len)
 	{
-		if (((const unsigned char *)src)[i] == c)
+		/*
+		 * We write this line at the first, cause:
+		 * even if src[i] = c or not, we should copy it to dst.
+		 */
+		((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
+		if (((const unsigned char *)src)[i] == (unsigned char)c)
 		{
-			((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
-			return (dst);
-		}
-		else
-		{
-			((unsigned char *)dst)[i] = ((const unsigned char *)src)[i];
+			/*
+			 * a pointer to the byte after the copy of the character(c) in the string 
+			 * (dst) is returned.
+			 */
+			return (dst + i + 1);
 		}
 		i++;
 	}
-	return (dst);
+	return (0);
 }
 
 int	main()
@@ -47,6 +57,8 @@ int	main()
 	printf("The string befor memccpy : %s\n", dst);
 	memccpy(dst, src, 't', 7);
 	printf("The string after: %s\n", dst);
+
+// comparing with the original one.
 
 	char dst1[] = ".......";
 	char src1[] = "Ghaiath";
