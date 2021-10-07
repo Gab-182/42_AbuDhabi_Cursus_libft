87fa3a29f9ca1275cbc4434e4f6d9c6f9e81a7ed
Gabdoush <ghaith181994@hotmail.com>
2021-10-07 23:26:20 +0400

    Correcting ft_strtrim() function, after testing it with /LibftTester/.

--- libft/Non-standard/ft_strtrim.c
+++ libft/Non-standard/ft_strtrim.c
@@ -7,86 +7,102 @@
  * specified in ’set’ removed from the beginning and the end of the string.
  */
 
-# include <stdio.h>
-# include <stdlib.h>
 # include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
 
-char	*ft_strtrim(char const *s1, char const *set)
+static	int check_for_set(char c, char const *set);
+
+/*
+* Note:
+* =====
+* remove this finction ft_substr() and include "libft.h"
+*/
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char *new_str;
 	size_t n;
-	size_t j;
 	size_t i;
+	char *sub_str;
 
-	new_str = (char *)malloc(strlen(s1) + 1);
-	n = 0;
-	j = 0;
+	n = start;
 	i = 0;
-	if (!s1 || !set || !new_str)
+	sub_str = (char *)malloc(len + 1);
+
+	if (!sub_str)
 	{
 		return (NULL);
 	}
-	while (set[i] != '\0' && n < strlen(s1) - 1)
+	while (n < strlen(s) && i < len)
 	{
-		if (s1[0] == set[i])
-		{
-			n++;
-			while (n < strlen(s1) - 1)
-			{
-				new_str[j] = s1[n];
-				n++;
-				j++;
-			}
-		}
+		sub_str[i] = ((char *)s)[n];
+		n++;
 		i++;
 	}
-	// That is means that we didn't found from the while loop any item 
-	// from the set that is equal to the first item in the string.
-	if (n == 0)
-	{
-		while (n < strlen(s1) - 1)
-		{
-			new_str[j] = s1[n];
-			n++;
-			j++;
-		}
-	}
-	i = 0;
-	// Checking for the last item in the string.
-	while (set[i] != '\0')
-	{
-		if (s1[strlen(s1) - 1] == set[i])
-		{
-			n++;
-			break;
-		}
-		i++;
-	}
-	// If the last item in the string not in (set), copy it to the new
-	// string, and return the string.
-	if(n == strlen(s1) - 1)
-		{
-			new_str[j] = s1[n];
-			j++;
-		}
-	new_str[j] = '\0';
-	return (new_str);
+	sub_str[i] = '\0';
+	return sub_str;
 }
 
-int	main()
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t start;
+	size_t end;
+
+	start = 0;
+	if (!s1)	
+		return (0);
+
+	/* Check the begining of the string characters.*/
+	while (s1[start] && check_for_set(s1[start], set))
+		start++;
+	
+	/* Check the end of the string characters.
+	 *
+ 	 * Note:
+ 	 * =====
+ 	 * Don't forgit to replace the strlen() function with ft_strlen() when you 
+ 	 * write youre project.
+ 	 */
+	end = strlen(s1);
+	while (end > start && check_for_set(s1[end - 1], set))
+		end--;
+	/*
+	* Use the function ft_substr(), to return a new string
+	* that end from the (start) point and have a max length
+	* of (end - start).
+	*/
+	return (ft_substr(s1, start, (end-start)));
+}
+
+/*
+ * check_for_set():
+ * ----------------
+ * To check each character from the string (s) if it is
+ *  found in the set of characters (set).
+ *  if the character is in the set return True (1),
+ *  if not return False (0).
+ */
+static	int check_for_set(char c, char const *set)
+{
+	size_t i;
+
+	i = 0;
+	while (set[i] != '\0')
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int main()
 {
 	char *s;
 	//char set[] = "";
-	char set[] = {'G', 'h', 'g', 't', 'i'};
-	s = "Ghaiath Abdoush";
+	char set[] = "Ggho";
+	//char set[] = {'G', 'h', 'g', 't', 'i'};
+	s = "GGgghhggghhhhhGhaiabdohhh";
 	printf("The result of ft_strtrim() is :%s\n", ft_strtrim(s, set));
 
 	return EXIT_SUCCESS;
 }
-
-/*
- * Note:
- * =====
- * Don't forgit to replace the strlen() function with ft_strlen() when you 
- * write youre project.
- */
