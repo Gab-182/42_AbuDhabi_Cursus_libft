330759631d104102ecbfce6592f6329deb9fbff9
Gabdoush <ghaith181994@hotmail.com>
2021-10-11 21:18:00 +0400

    add ft_lstdelone() after testing.

---
+++ libft/Linked-list/ft_lstdelone.c
@@ -0,0 +1,43 @@
+/**
+ * Takes as a parameter an element and frees the memory of the element’s content 
+ * using the function ’del’ given as a parameter and free the element.
+ * The memory of ’next’ must not be freed.
+ * 
+ * #1. The element to free.
+ * #2. The address of the function used to delete the content.
+ * -----------------------------------------------------------------------
+ * Notes:
+ * =====
+ * free(), works on the [Heap memory], not on the [Stack memory].
+ * 
+ * You can only free the memory from the pointer to it that returned 
+ * by malloc.
+ * 
+*/
+
+#include <stdlib.h>
+
+typedef struct s_list
+{
+    void *content;
+    struct s_list *next;
+} t_list;
+
+/*------------------------------------------------------------------------*/
+
+/***
+ * • content : The data contained in the element. The void * allows to store any kind
+ *	of data.
+ * • next : The next element’s address or NULL if it’s the last element.
+*/
+
+/*--------------------------Required function----------------------------*/
+void ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+    if (lst != NULL)
+    {
+        del(lst -> content);
+        free(lst);
+    }
+
+}
